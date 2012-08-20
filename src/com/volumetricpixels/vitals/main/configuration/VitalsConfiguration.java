75e727e203f8e3495f17afae102e42df580dd21d
Kohle <kohle@mrkohle.com>
2012-08-20 17:47:21 -0400

    Started to work on the config.
    Signed-off-by: Kohle <kohle@mrkohle.com>

--- src/com/volumetricpixels/vitals/main/configuration/VitalsConfiguration.java
+++ src/com/volumetricpixels/vitals/main/configuration/VitalsConfiguration.java
@@ -7,7 +7,7 @@ import org.spout.api.util.config.ConfigurationHolderConfiguration;
 import org.spout.api.util.config.yaml.YamlConfiguration;
 
 public class VitalsConfiguration extends ConfigurationHolderConfiguration {
-	public static final ConfigurationHolder NameOfValueHere = new ConfigurationHolder("Value", "path", "to");
+	public static final ConfigurationHolder NameOfValueHere = new ConfigurationHolder("Value", "path", "to Value");
 
 	public VitalsConfiguration(File dataFolder) {
 		super(new YamlConfiguration(new File(dataFolder, "config.yml")));
