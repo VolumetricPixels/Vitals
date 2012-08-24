a769d60ca2bae0d7df004b60291b86896dcb5e69
Kohle <kohle@mrkohle.com>
2012-08-24 15:38:52 -0400

    Fixed the colors.
    Signed-off-by: Kohle <kohle@mrkohle.com>

--- src/com/volumetricpixels/vitals/main/commands/AdminCommands.java
+++ src/com/volumetricpixels/vitals/main/commands/AdminCommands.java
@@ -19,22 +19,29 @@ import org.spout.vanilla.entity.world.VanillaSky;
  */
 public class AdminCommands {
 
-	@Command(aliases = {"weather"}, usage = "<weather type>", desc = "Change or check the weather.", min = 1, max = 1)
+	@Command(aliases = {"weather"}, usage = "<weather type>", desc = "Change the weather.", min = 1, max = 1)
 	@CommandPermissions("vitals.weather")
 	public void weather(CommandContext args, CommandSource source) throws CommandException {
 		if (source instanceof Player) {
+			//Getting the sky
 			VanillaSky sky = VanillaSky.getSky(((Player) source).getWorld());
 
 			// Get the weather
 			String wName = args.getString(0, Weather.CLEAR.name());
 			Weather w = Weather.valueOf(wName.toUpperCase());
 			if (w == null) {
-				source.sendMessage(ChatStyle.GRAY, "Unknown weather type '" + wName + "'. Available weather types: " + Joiner.on(' ').join(Weather.values()));
+				source.sendMessage(ChatStyle.GRAY, "Unknown weather type '" + wName + "'. Available weather types: " + ChatStyle.RED, Joiner.on(' ').join(Weather.values()));
 				return;
 			}
 
 			sky.setWeather(w);
-			source.sendMessage(ChatStyle.BRIGHT_GREEN, "Weather set to '" + StringUtils.capitalize(w.toString()) + "'.");
+			source.sendMessage(ChatStyle.GRAY, "Weather set to '" + ChatStyle.RED, StringUtils.capitalize(w.toString()) + "'.");
 		}
+	}
+	
+	@Command(aliases = {"time"}, usage = "<time>", desc = "Change the time.", min = 1, max =1)
+	@CommandPermissions("vitals.time")
+	public void time(CommandContext args, CommandSource source) throws CommandException {
+		//TODO: Finish the time command.
 	}
 }
