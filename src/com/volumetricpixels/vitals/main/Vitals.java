b86f7ea8bf7abdc8927ac7228120ba02589fd53e
Ollie <dzineit@live.co.uk>
2012-08-31 10:40:25 +0200

    Cleaned up Vitals.java to follow VP standards:
    Newlines are at a 200 column limit.
    Spaces not tabs!
    
    Also:
    Removed unused import.
    Spout does not put the plugin name automatically in log messages.
    
    Signed-off-by: Ollie <dzineit@live.co.uk>

--- src/com/volumetricpixels/vitals/main/Vitals.java
+++ src/com/volumetricpixels/vitals/main/Vitals.java
@@ -1,6 +1,5 @@
 package com.volumetricpixels.vitals.main;
 
-import org.spout.api.UnsafeMethod;
 import org.spout.api.command.annotated.AnnotatedCommandRegistrationFactory;
 import org.spout.api.command.annotated.SimpleInjector;
 import org.spout.api.plugin.CommonPlugin;
@@ -11,27 +10,27 @@ import com.volumetricpixels.vitals.main.configuration.VitalsConfiguration;
 
 public class Vitals extends CommonPlugin {
 
-	private VitalsConfiguration config;
+        private VitalsConfiguration config;
 
-	@Override
-	public void onDisable() {
-		getLogger().info("disabled.");
-	}
+        @Override
+        public void onDisable() {
+                getLogger().info("[Vitals] v" + getDescription().getVersion() + " disabled!");
+        }
 
-	@Override
-	public void onEnable() {
-		config = new VitalsConfiguration(getDataFolder());
+        @Override
+        public void onEnable() {
+                config = new VitalsConfiguration(getDataFolder());
 
 		// Register commands.
-		AnnotatedCommandRegistrationFactory commandRegistration =
-				new AnnotatedCommandRegistrationFactory(new SimpleInjector(this));
-		getEngine().getRootCommand().addSubCommands(this, AdminCommands.class, commandRegistration);
-		getEngine().getRootCommand().addSubCommands(this, GeneralCommands.class, commandRegistration);
+                AnnotatedCommandRegistrationFactory commandRegistration = new AnnotatedCommandRegistrationFactory(new SimpleInjector(this));
+                getEngine().getRootCommand().addSubCommands(this, AdminCommands.class, commandRegistration);
+                getEngine().getRootCommand().addSubCommands(this, GeneralCommands.class, commandRegistration);
 
-		getLogger().info("v" + getDescription().getVersion() + " enabled.");
-	}
+                getLogger().info("[Vitals] v" + getDescription().getVersion() + " enabled!");
+        }
 
-	public VitalsConfiguration getConfig() {
-		return config;
-	}
+        public VitalsConfiguration getConfig() {
+                return config;
+        }
+
 }
