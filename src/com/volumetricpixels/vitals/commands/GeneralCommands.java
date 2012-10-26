dca698e215cf9dcdce2dda4150914d59602d2fdd
Kohle <kohle@mrkohle.com>
2012-10-25 21:28:50 -0400

    Added whereami
    Signed-off-by: Kohle <kohle@mrkohle.com>

--- src/com/volumetricpixels/vitals/commands/GeneralCommands.java
+++ src/com/volumetricpixels/vitals/commands/GeneralCommands.java
@@ -1,7 +1,17 @@
 package com.volumetricpixels.vitals.commands;
 
+import org.spout.api.command.CommandContext;
+import org.spout.api.command.CommandSource;
+import org.spout.api.command.annotated.Command;
+import org.spout.api.command.annotated.CommandPermissions;
+import org.spout.api.exception.CommandException;
+
 public class GeneralCommands {
 	
-    // TODO: General commands
+	@Command(aliases = { "whereami" }, usage = "[player]", desc = "Information on your location or [player].", min = 0, max = 1)
+    @CommandPermissions("vitals.general.whereami")
+    public void mute(CommandContext args, CommandSource source) throws CommandException {
+        // TODO: Finish whereami
+    }
 	
 }
